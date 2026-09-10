00339524abb1e2063a2e8fef9364b0c5cb2a1999
jj-streeter <judas.streeter@gmail.com>
2026-09-10 14:55:42 -0500

    Delete games/snake directory
    changed to /snake

--- games/snake/components/ScoreReaderComponent.js
+++
@@ -1,7 +0,0 @@
-class ScoreReaderComponent extends Component {
-    update(){
-        this.parent.getComponent("Text").text = "Score: " + Globals.score
-    }
-}
-  
-window.ScoreReaderComponent = ScoreReaderComponent
--- games/snake/prefabs/BodySegmentGameObject.js
+++
@@ -1,8 +0,0 @@
-class BodySegmentGameObject extends GameObject{
-    constructor(name = "BodySegmentGameObject"){
-        super(name)
-        this.addComponent(new Rectangle("black"))
-    }
-}
-
-window.BodySegmentGameObject = BodySegmentGameObject
